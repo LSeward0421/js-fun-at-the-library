406559c36d42202668c5fcb33bf86a16b3c83d3f
Lauren Seward <lauren.unal@gmail.com>
2022-12-01 14:00:07 -0600

    Save review

--- src/book.js
+++ src/book.js
@@ -1,11 +1,34 @@
-function createTitle() {
-  
-}
+const { assert } = require("chai");
+
+function createTitle(title) {
+  return `The ${title}`;
+};
+
+function buildMainCharacter(name, age, pronouns) {
+  var character = {
+    name: `${name}`,
+    age: `${age}`,
+    pronouns: `${pronouns}`
+  };
+  return character;
+  };
+
+function saveReview(newComment, reviewsArray) {
+  if (reviewsArray.includes(newComment) === false) {
+    return reviewsArray.push(newComment);
+  }
+};
+
+
+
+
+
+
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+  buildMainCharacter,
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
