e69e7994f81e8cc7152fefad19ca2c6d06fac885
Lauren Seward <lauren.unal@gmail.com>
2022-12-01 20:44:31 -0600

    Unshelf book

--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,35 @@
+function shelfBook(book, shelf) {
+  if (shelf.length <= 2) {
+  return shelf.unshift(book);
+}
+};
+
+function unshelfBook(bookTitle, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+  if (shelf[i].title === bookTitle) {
+    return shelf.splice(i, 1)
+   }
+}
+}
+
+// function listTitles(shelf) {
+// for (var i = 0; i < shelf.length; i++) {
+
+// }
+// };
+
+
+
+
+
+
+
+
+
 
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
+  shelfBook,
+  unshelfBook,
+  listTitles,
   // searchShelf
 };
