f2d347387496ea7999e4818d62f38a59f8573ccc
Lauren Seward <lauren.unal@gmail.com>
2022-12-02 17:15:40 -0600

    Create library

--- src/Library.js
+++ src/Library.js
@@ -1,7 +1,20 @@
+function createLibrary(libraryName) {
+return library = {
+  name: libraryName,
+  shelves: {fiction:[], fantasy:[], nonFiction:[]}
+};
+}
+
+// function addBook(currentLibrary, bookTitle) {
+
+// }
+
+
+
 
 
 module.exports = {
-  // createLibrary,
-  // addBook,
+  createLibrary,
+  addBook,
   // checkoutBook
 };
