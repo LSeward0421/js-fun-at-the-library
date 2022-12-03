b3d7a279f26373750d0528f4320034ebc4c78736
Lauren Seward <lauren.unal@gmail.com>
2022-12-02 19:25:45 -0600

    Create Library and add book

--- src/Library.js
+++ src/Library.js
@@ -5,16 +5,28 @@ return library = {
 };
 }
 
-// function addBook(currentLibrary, bookTitle) {
+function addBook(library, bookTitle) {
+  library.shelves[bookTitle.genre].push(bookTitle)
+};
 
+
+function checkoutBook(library, title, genre) {
+  var chosenBook = library.length.shelves[title.genre]
+ for (var i = 0; i < library.length.shelves[title.genre]; i++) {
+  if (library.shelves[title.genre] === title)
+  return library.length.shelves
+ }
+}
+// function unshelfBook(bookTitle, shelf) {
+//   for (var i = 0; i < shelf.length; i++) {
+//   if (shelf[i].title === bookTitle) {
+//     return shelf.splice(i, 1)
+//    }
 // }
-
-
-
-
+// }
 
 module.exports = {
   createLibrary,
   addBook,
-  // checkoutBook
+  checkoutBook
 };
