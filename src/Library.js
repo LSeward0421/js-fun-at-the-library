224ed90fb67a05a9a333ee1a646bfcbde1264ac3
Lauren Seward <lauren.unal@gmail.com>
2022-12-03 13:59:01 -0600

    Check out book

--- src/Library.js
+++ src/Library.js
@@ -11,19 +11,15 @@ function addBook(library, bookTitle) {
 
 
 function checkoutBook(library, title, genre) {
-  var chosenBook = library.length.shelves[title.genre]
- for (var i = 0; i < library.length.shelves[title.genre]; i++) {
-  if (library.shelves[title.genre] === title)
-  return library.length.shelves
+  var chosenShelf = library['shelves'][genre]
+ for (var i = 0; i < chosenShelf.length; i++) {
+  if (chosenShelf[i].title === title) {
+  chosenShelf.splice(i, 1)
+  return `You have now checked out ${title} from the ${library.name}`
  }
 }
-// function unshelfBook(bookTitle, shelf) {
-//   for (var i = 0; i < shelf.length; i++) {
-//   if (shelf[i].title === bookTitle) {
-//     return shelf.splice(i, 1)
-//    }
-// }
-// }
+return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+}
 
 module.exports = {
   createLibrary,
